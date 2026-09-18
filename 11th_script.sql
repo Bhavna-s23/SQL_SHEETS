6c0adca409704e5ec8ff8daf871e3af3ed85286d
Bhavna Sharma <baaa74318@gmail.com>
2026-09-18 23:39:18 +0530

    Add files via upload

---
+++ 11th_script.sql
@@ -0,0 +1,11 @@
+SELECT * FROM shruu.customer;
+select 
+id ,
+name,
+gender,
+salary,
+salary+1111 as increased_salary ,
+salary+1111 - salary
+from shruu.customer;
+
+#PEMDAS (paranthesis , exponent , multiplication , division ,addition , substraction) 
